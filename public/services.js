d85f4276bbfa0806a1813f906737c821b8e98851
MKS-Student <mks.student@hackreactor.com>
2016-02-08 15:52:30 -0800

    added angular and bower files

---
+++ public/services.js
@@ -0,0 +1,7 @@
+console.log('hello');
+
+var app = angular.module('lolStats',[]);
+
+var mainController = function ($scope,$http){
+  $scope.test = 473;
+}
--- server.js
+++ server.js
@@ -3,20 +3,25 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
 var app = express();
-
 var port = process.env.PORT || 3000;
-//app.user(express.static(__dirname + '/views'));
+
+
+///template stuff
+app.use(express.static(__dirname + '/public'));
 app.set('views',__dirname + '/views');
 app.set('view engine', 'jade')
+//
+// logger
+app.use(morgan('dev'));
 
-
-console.log("Sever now listening on port " + port);
-
-
+////
 app.get('/', function (req , res){
   console.log('sent a get reqest to /')
-  res.render('index', {test : 'hi'}); 
+  res.render('index'); 
   //res.send('helloWorld')
 });
+
+////////////
+console.log("Sever now listening on port " + port);
 
 app.listen(port);
