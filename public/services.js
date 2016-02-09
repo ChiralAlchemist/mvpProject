0a7dde8c9bea1b4bc2a425d32265d23cb188b776
MKS-Student <mks.student@hackreactor.com>
2016-02-09 12:20:00 -0800

    added select wheel

--- public/services.js
+++ public/services.js
@@ -3,13 +3,15 @@
 angular.module('lolStats',[])
 .controller('mainController', function($scope,$http){
   $scope.inGameName = '';
-  
+  $scope.season = 2016;
+
+
   $scope.champList = [];
-  var sendToSever= function(inGameName){
+  var sendToSever= function(inGameName,season){
     $http({
       method: "POST",
       url: 'http://localhost:3000/',
-      data : {inGameName: inGameName}
+      data : {inGameName: inGameName, season: season}
     }).then( function(res){
       console.log('was successful');
       console.log(res);
@@ -21,12 +23,14 @@ angular.module('lolStats',[])
   };
 
   var makeImageUrl = function (champName){
-    var template = 'http://ddragon.leagueoflegends.com/cdn/6.2.1/img/champion/'+champName+'.png';
-    return template;
+    if(champName){
+      var template = 'http://ddragon.leagueoflegends.com/cdn/6.2.1/img/champion/'+champName+'.png';
+      return template;
+    }
   };
 
   $scope.sendIGN= function(){ 
-    sendToSever($scope.inGameName);
+    sendToSever($scope.inGameName,$scope.season);
   }
   $scope.makeImageUrl= makeImageUrl;
 })
