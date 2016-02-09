7b835f5acf9bd6dd66b6e2f95bc90c520c2f6bed
MKS-Student <mks.student@hackreactor.com>
2016-02-08 17:07:43 -0800

    client talks to server

--- public/services.js
+++ public/services.js
@@ -1,7 +1,31 @@
-console.log('hello');
 
-var app = angular.module('lolStats',[]);
 
-var mainController = function ($scope,$http){
-  $scope.test = 473;
-}
+angular.module('lolStats',[])
+.controller('mainController', function($scope,$http){
+  $scope.inGameName = '';
+  
+
+  var sendToSever= function(inGameName){
+    $http({
+      method: "POST",
+      url: 'http://localhost:3000/',
+      data : {inGameName: inGameName}
+    }).then( function(res){
+      console.log('was successful');
+      console.log(res);
+      return res.data;
+    }, function (err){
+      console.log(err)
+    });
+    console.log('attempted to send ign');
+  };
+
+  $scope.sendIGN= function(){ 
+    return sendToSever($scope.inGameName);
+  }
+})
+
+.factory('connectServer', function($http){
+
+  
+});
--- server.js
+++ server.js
@@ -5,8 +5,10 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 3000;
 
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 ///template stuff
+
 app.use(express.static(__dirname + '/public'));
 app.set('views',__dirname + '/views');
 app.set('view engine', 'jade')
@@ -21,7 +23,16 @@ app.get('/', function (req , res){
   //res.send('helloWorld')
 });
 
+app.post('/', function(req, res){
+  console.log(req.body);
+
+  res.send("heroku")
+});
+
 ////////////
 console.log("Sever now listening on port " + port);
 
 app.listen(port);
+
+//heroku stuff
+//https://salty-peak-44142.herokuapp.com/ | https://git.heroku.com/salty-peak-44142.git
