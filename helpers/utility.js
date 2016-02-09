b22d993c1fd2c3c5e8c5672505982bb3135b1db1
MKS-Student <mks.student@hackreactor.com>
2016-02-09 11:08:44 -0800

    now shows champion pics

--- helpers/utility.js
+++ helpers/utility.js
@@ -1,5 +1,5 @@
 var request = require('request');
-
+var championKeys = require('./championKeys.js')
 var riotApiKey = 'ff62241d-f02d-443b-8309-c4b10a4bc446';
 
 exports.getPlayerIdAndIcon = function (sentInGameName, idCallback){
@@ -21,17 +21,26 @@ exports.getPlayerStats = function (sentInGameName,statCallback){
 
   exports.getPlayerIdAndIcon(sentInGameName, function(nameAndId){
     var gameId = nameAndId.id;
-    
+      
 
-    var playerStatUrl = 'https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/'+gameId+'/ranked?season=SEASON2016&api_key=ff62241d-f02d-443b-8309-c4b10a4bc446'
+    var playerStatUrl = 'https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/'+gameId+'/ranked?season=SEASON2015&api_key=ff62241d-f02d-443b-8309-c4b10a4bc446'
     
     request(playerStatUrl,function (err, res, playerStats){
       var parsedPlayerStats = JSON.parse(playerStats);
       var mostPlayed = [];
-      for(var i=0; i<2; i++){
-        mostPlayed.push(parsedPlayerStats.champions[i]);
+      var top3 = parsedPlayerStats.champions.slice(parsedPlayerStats.champions.length-3);
+      for(var i=0; i<parsedPlayerStats.champions.length; i++){
+
+        var championObj = parsedPlayerStats.champions[i];
+        //console.log(parsedPlayerStats.champions[i]);
+        championObj.championName = championKeys.keys[championObj.id]
+        mostPlayed.push(championObj);
       }
       statCallback(mostPlayed);
     });  
   });
+}
+
+var findMostPlayed = function(championObjList){
+  //var 
 }
--- public/services.js
+++ public/services.js
@@ -4,7 +4,7 @@ angular.module('lolStats',[])
 .controller('mainController', function($scope,$http){
   $scope.inGameName = '';
   
-
+  $scope.champList = [];
   var sendToSever= function(inGameName){
     $http({
       method: "POST",
@@ -13,17 +13,24 @@ angular.module('lolStats',[])
     }).then( function(res){
       console.log('was successful');
       console.log(res);
-      return res.data;
+       $scope.champList = res.data;
     }, function (err){
       console.log(err)
     });
     console.log('attempted to send ign');
   };
 
+  var makeImageUrl = function (champName){
+    var template = 'http://ddragon.leagueoflegends.com/cdn/6.2.1/img/champion/'+champName+'.png';
+    return template;
+  };
+
   $scope.sendIGN= function(){ 
-    return sendToSever($scope.inGameName);
+    sendToSever($scope.inGameName);
   }
+  $scope.makeImageUrl= makeImageUrl;
 })
+
 
 .factory('connectServer', function($http){
 
