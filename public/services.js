3ee6d5da882cde538d5e98eaf4335a63e73f2604
MKS-Student <mks.student@hackreactor.com>
2016-02-09 11:19:36 -0800

    made connection secure

--- public/services.js
+++ public/services.js
@@ -6,7 +6,7 @@ angular.module('lolStats',[])
   
   $scope.champList = [];
   var sendToSever= function(inGameName){
-    $http({
+    $https({
       method: "POST",
       url: 'https://salty-peak-44142.herokuapp.com/' , //'http://localhost:3000/',
       data : {inGameName: inGameName}
